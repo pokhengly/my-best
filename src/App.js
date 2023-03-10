7177b9d29cc5bae39a994e7fd4390b367a80988e
pokhengly <henglypok21012021@gmail.com>
2023-03-10 17:15:02 +0700

    Learn

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
+import React, { Component} from "react";
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Greet from './componets/Greet'
+import Welcome from './componets/Welcome';
+import Hello from "./componets/Hello";
+import Message from "./componets/Message";
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Message/>
+                {/*<Greet name="Bruce"  heroName="Batman">*/}
+                {/*    <p>This is children props</p>*/}
+                {/*</Greet>*/}
+                {/*<Greet name="Clark"  heroName="Superman">*/}
+                {/*    <button>Action</button>*/}
+                {/*</Greet>*/}
+                {/*<Greet name="Diana"  heroName="Wonder Woman"/>*/}
+                {/*<Welcome name="Bruce"  heroName="Batman"/>*/}
+                {/*<Welcome name="Clark"  heroName="Superman"/>*/}
+                {/*<Welcome name="Diana"  heroName="Wonder Woman"/>*/}
+                {/* <Hello /> */}
+            </div>
+        )
+    }
 }
-
 export default App;
